aa5436fff0cdf35f833cd92157488a852c927c7d
rafinhaa <rafinhaa@me.com>
2022-10-24 10:05:09 -0300

    Design System em React - Configuração do Stitches

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,6 +1,12 @@
-import { colors } from '@ignite-ui/tokens'
-import React from 'react'
+import { styled } from './styles'
+
+const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite300',
+  borderRadius: '$md',
+  padding: '$4',
+})
 
 export function App() {
-  return <h1 style={{ color: colors.ignite300 }}>Hello World</h1>
+  return <Button>Hello World</Button>
 }
