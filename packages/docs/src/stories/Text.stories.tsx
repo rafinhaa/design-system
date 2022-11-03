f585843a1575b6e93fe82c93fa7793874109ccea
rafinhaa <rafinhaa@me.com>
2022-11-03 10:56:37 -0300

    Componentes do Design System - Componente: Text

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children: 'lorem ipsum, dolor isuet',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
