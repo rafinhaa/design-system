554bb34e105155fffd8a95aeee1df3e421a5e8dd
rafinhaa <rafinhaa@me.com>
2022-11-15 11:54:16 -0300

    Publicação e manutenção - Configurando Changesets

--- packages/docs/src/stories/TextArea.stories.tsx
+++ packages/docs/src/stories/TextArea.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { Box, Text, TextArea, TextAreaProps } from '@ignite-ui/react'
+import { Box, Text, TextArea, TextAreaProps } from '@codeheart/react'
 
 export default {
   title: 'Form/TextArea',
--- packages/react/src/styles/index.ts
+++ packages/react/src/styles/index.ts
@@ -6,7 +6,7 @@ import {
   lineHeights,
   radii,
   space,
-} from '@ignite-ui/tokens'
+} from '@codeheart/tokens'
 import { createStitches, defaultThemeMap } from '@stitches/react'
 
 export const {
